e93132a6676865f98c9e4cc53060d6b037357f76
Anna <anna.fedorova@simbirsoft.com>
2024-07-03 16:53:33 +0300

    готовый тест на добавление новой записи в адресную книгу

---
+++ addressbook.py
@@ -0,0 +1,28 @@
+class Addressbook:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, phonehome,
+                               mobile, phonework, fax, email, email2, email3, homepage, bday, bmonth, byear, aday,
+                               amonth, ayear, address2, home, note):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phonehome = phonehome
+        self.mobile = mobile
+        self.phonework = phonework
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.home = home
+        self.note = note
