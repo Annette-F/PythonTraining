51a915dc85311aa1e2e2ac70991a578e2d7f020e
Anna <anna.fedorova@simbirsoft.com>
2024-07-03 09:53:03 +0300

    исправлено название локаторов

--- test_add_group.py
+++ test_add_group.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from group import Group
 
@@ -18,38 +14,38 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/addressbook/")
 
     def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
 
     def create_group(self, wd, group):
         # init group creation
-        wd.find_element_by_name("new").click()
+        wd.find_element(By.NAME, "new").click()
         # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
-        wd.find_element_by_name("submit").click()
+        wd.find_element(By.NAME, "submit").click()
 
     def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
 
     def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def test_add_group(self):
         wd = self.wd
@@ -69,20 +65,6 @@ class test_add_group(unittest.TestCase):
         self.return_to_group_page(wd)
         self.logout(wd)
 
-
-    # def is_element_present(self, how, what):
-    #     try:
-    #         self.wd.find_element(by=how, value=what)
-    #     except NoSuchElementException as e:
-    #         return False
-    #     return True
-    #
-    # def is_alert_present(self):
-    #     try:
-    #         self.wd.switch_to_alert()
-    #     except NoAlertPresentException as e:
-    #         return False
-    #     return True
 
     def tearDown(self):
         self.wd.quit()
