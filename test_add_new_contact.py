bfed0a36e606478aeee95db317965430a115a744
Anna <anna.fedorova@simbirsoft.com>
2024-07-04 11:06:00 +0300

    вспомогательные модули вынесены в отдельный класс Adbook_application

--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -6,133 +6,25 @@ from selenium.webdriver.support.ui import Select
 import unittest
 from selenium.webdriver.common.by import By
 from addressbook import Addressbook
+from adbook_application import Adbook_application
+
 
 class TestAddNewContact(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_add_new_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "add new").click()
-
-    def add_address_book_entry(self, addressbook):
-        wd = self.wd
-        self.open_add_new_page()
-        # fill address book entry
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(addressbook.firstname)
-        wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(addressbook.middlename)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(addressbook.lastname)
-        wd.find_element(By.NAME, "nickname").click()
-        wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys(addressbook.nickname)
-        wd.find_element(By.XPATH, "//input[@type='file']").send_keys(os.path.join(os.getcwd(), 'photo.jpg'))
-        wd.find_element(By.NAME, "title").click()
-        wd.find_element(By.NAME, "title").clear()
-        wd.find_element(By.NAME, "title").send_keys(addressbook.title)
-        wd.find_element(By.NAME, "theform").click()
-        wd.find_element(By.NAME, "company").click()
-        wd.find_element(By.NAME, "company").clear()
-        wd.find_element(By.NAME, "company").send_keys(addressbook.company)
-        wd.find_element(By.NAME, "address").click()
-        wd.find_element(By.NAME, "address").clear()
-        wd.find_element(By.NAME, "address").send_keys(addressbook.address)
-        wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "home").clear()
-        wd.find_element(By.NAME, "home").send_keys(addressbook.phonehome)
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(addressbook.mobile)
-        wd.find_element(By.NAME, "work").click()
-        wd.find_element(By.NAME, "work").clear()
-        wd.find_element(By.NAME, "work").send_keys(addressbook.phonework)
-        wd.find_element(By.NAME, "fax").click()
-        wd.find_element(By.NAME, "fax").clear()
-        wd.find_element(By.NAME, "fax").send_keys(addressbook.fax)
-        wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").clear()
-        wd.find_element(By.NAME, "email").send_keys(addressbook.email)
-        wd.find_element(By.NAME, "email2").click()
-        wd.find_element(By.NAME, "email2").clear()
-        wd.find_element(By.NAME, "email2").send_keys(addressbook.email2)
-        wd.find_element(By.NAME, "email3").click()
-        wd.find_element(By.NAME, "email3").clear()
-        wd.find_element(By.NAME, "email3").send_keys(addressbook.email3)
-        wd.find_element(By.NAME, "homepage").click()
-        wd.find_element(By.NAME, "homepage").clear()
-        wd.find_element(By.NAME, "homepage").send_keys(addressbook.homepage)
-        wd.find_element(By.NAME, "bday").click()
-        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(addressbook.bday)
-        wd.find_element(By.XPATH, "//option[@value='17']").click()
-        wd.find_element(By.NAME, "bmonth").click()
-        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(addressbook.bmonth)
-        wd.find_element(By.XPATH, "//option[@value='November']").click()
-        wd.find_element(By.NAME, "byear").click()
-        wd.find_element(By.NAME, "byear").clear()
-        wd.find_element(By.NAME, "byear").send_keys(addressbook.byear)
-        wd.find_element(By.NAME, "aday").click()
-        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(addressbook.aday)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[3]/option[22]").click()
-        wd.find_element(By.NAME, "amonth").click()
-        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(addressbook.amonth)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[4]/option[9]").click()
-        wd.find_element(By.NAME, "ayear").click()
-        wd.find_element(By.NAME, "ayear").clear()
-        wd.find_element(By.NAME, "ayear").send_keys(addressbook.ayear)
-        wd.find_element(By.NAME, "address2").click()
-        wd.find_element(By.NAME, "address2").clear()
-        wd.find_element(By.NAME, "address2").send_keys(addressbook.address2)
-        wd.find_element(By.NAME, "phone2").click()
-        wd.find_element(By.NAME, "phone2").clear()
-        wd.find_element(By.NAME, "phone2").send_keys(addressbook.home)
-        wd.find_element(By.NAME, "notes").click()
-        wd.find_element(By.NAME, "notes").clear()
-        wd.find_element(By.NAME, "notes").send_keys(addressbook.note)
-        # enter address book entry
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
-        self.return_home_page()
-
-    def return_home_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "home page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+        self.adbook_app = Adbook_application()
 
     def test_add_new_contact(self):
-        self.login("admin", "secret")
-        self.add_address_book_entry(Addressbook("Alexander", "Sergeevich", "Pyshkin", "Poet", "Title", "Company LLC",
+        self.adbook_app.login("admin", "secret")
+        self.adbook_app.add_address_book_entry(Addressbook("Alexander", "Sergeevich", "Pyshkin", "Poet", "Title", "Company LLC",
                                     "Saint-Petersburg", "1234567890", "9876543210", "1472583690", "3692580147",
                                     "alex@mal.com", "alex2@mail.com", "alex3@mail.com", "http://www.pyshkin.com", "17",
                                     "November", "1994", "20", "August", "1999", "Moscow", "my home", "note"))
-        self.logout()
+        self.adbook_app.logout()
 
 
 
     def tearDown(self):
-        self.wd.quit()
+        self.adbook_app.adbook_destroy()
 
 
 if __name__ == "__main__":
