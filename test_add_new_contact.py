c0eed1f6c332473ea719bbf48d16f5b0042d2f0c
Anna <anna.fedorova@simbirsoft.com>
2024-07-04 11:18:49 +0300

    создание фикстуры

--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -1,31 +1,21 @@
 # -*- coding: utf-8 -*-
-import os
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
-from selenium.webdriver.common.by import By
+import pytest
 from addressbook import Addressbook
 from adbook_application import Adbook_application
 
 
-class TestAddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.adbook_app = Adbook_application()
-
-    def test_add_new_contact(self):
-        self.adbook_app.login("admin", "secret")
-        self.adbook_app.add_address_book_entry(Addressbook("Alexander", "Sergeevich", "Pyshkin", "Poet", "Title", "Company LLC",
-                                    "Saint-Petersburg", "1234567890", "9876543210", "1472583690", "3692580147",
-                                    "alex@mal.com", "alex2@mail.com", "alex3@mail.com", "http://www.pyshkin.com", "17",
-                                    "November", "1994", "20", "August", "1999", "Moscow", "my home", "note"))
-        self.adbook_app.logout()
+@pytest.fixture()
+def adbook_app(request):
+    adbook_fixture = Adbook_application()
+    request.addfinalizer(adbook_fixture.adbook_destroy)
+    return adbook_fixture
 
 
-
-    def tearDown(self):
-        self.adbook_app.adbook_destroy()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_new_contact(adbook_app):
+    adbook_app.login("admin", "secret")
+    adbook_app.add_address_book_entry(Addressbook("Alexander", "Sergeevich", "Pyshkin", "Poet", "Title", "Company LLC",
+                                "Saint-Petersburg", "1234567890", "9876543210", "1472583690", "3692580147",
+                                "alex@mal.com", "alex2@mail.com", "alex3@mail.com", "http://www.pyshkin.com", "17",
+                                "November", "1994", "20", "August", "1999", "Moscow", "my home", "note"))
+    adbook_app.logout()
