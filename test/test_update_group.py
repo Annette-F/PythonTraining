0a8914579f4b5365642add0298f7302f8aefe7b7
Anna <anna.fedorova@simbirsoft.com>
2024-07-31 15:11:02 +0300

    Вынос login и logout в фикстуру

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -2,6 +2,4 @@ from model.group import Group
 
 
 def test_update_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.update(Group(name="name2", header="header2", footer="footer2"))
-    app.session.logout()
