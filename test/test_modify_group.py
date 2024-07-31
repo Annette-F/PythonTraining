610c2c7fa50936f5b72f4fd20c29502567edca15
Anna <anna.fedorova@simbirsoft.com>
2024-07-31 14:44:52 +0300

    Добавлен тест test_modify_first_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,11 +3,11 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="New name"))
+    app.group.modify_first_group(Group(name="New name"))
     app.session.logout()
 
 
 def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(header="New header"))
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
